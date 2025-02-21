c335c644d675ad34dbd8fef3da3bdc0150fd5243
Chonatan <njralonso@outlook.com>
2025-02-21 19:13:35 +0100

    eliminados los componentes del drag&drop por incompatibilidad

--- client/src/components/Task.jsx
+++ client/src/components/Task.jsx
@@ -1,18 +1,9 @@
-import { useDrag } from "react-dnd";
-
 const Task = () => {
-  const [{ isDragging }, drag, dragPreview] = useDrag(() => ({
-    // "type" is required. It is used by the "accept" specification of drop targets.
-    type: 'image',
-    // The collect function utilizes a "monitor" instance (see the Overview for what this is)
-    // to pull important pieces of state from the DnD system.
-    collect: (monitor) => ({
-      isDragging: monitor.isDragging()
-    })
-  }))
   return (
     <>
-      <img id="1" ref={drag} src="https://placecats.com/300/200" alt="" style={{ border: isDragging ? "5px solid pink" : "0px" }} />
+      <div>
+        <h3>sdaf</h3>
+      </div>
     </>
   )
 }
--- client/src/views/Board.jsx
+++ client/src/views/Board.jsx
@@ -1,21 +1,10 @@
 import Layout from "./layouts/_layout"
-import { useParams, useSearchParams } from 'react-router'
-import { useDrop } from "react-dnd"
+import { useParams } from 'react-router'
 import Task from "../components/Task"
-import { useState } from "react"
 
 const Board = () => {
   const { name } = useParams()
-  const [{ isOver }, collectedProps, drop] = useDrop(() => ({
-    accept: "image",
-    drop: () => addImageToBoard(),
-    collect: (monitor) => ({
-      isOver: monitor.isOver()
-    })
-  }))
-  const addImageToBoard = () => {
-    console.log("ok")
-  }
+
   return (
     <Layout>
       <h1 className="bg-red-300 text-white font-semibold text-5xl">OLA K ASE {name}</h1>
@@ -30,12 +19,10 @@ const Board = () => {
           gridTemplateColumns: "repeat(3, 1fr)",
           gridGap: "10px",
           gridAutoRows: "minmax(100px, auto)",
-          // opacity: isDragging ? 0.5 : 1
         }}>
           <Task />
           <Task />
         </ul>
-        <div ref={drop} style={{ border: "2px solid black", height: "300px" }}></div>
       </div>
     </Layout>
   )
